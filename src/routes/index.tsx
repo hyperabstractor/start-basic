4745ab39e37b0784732ad122cb006ae671e21226
Hyper <hyperabstractor@gmail.com>
2024-03-16 18:45:41 +0530

    start

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        <div class="flex justify-center items-center">
+          <span class="mr-5">Home</span>
+        </div>
+      </h1>
+    </main>
+  );
+}
